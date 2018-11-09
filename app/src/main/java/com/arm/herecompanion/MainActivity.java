3679693cc29e2637c14118dc02568f79709dc065
Armando <luis.armando.amador.duran@gmail.com>
2018-11-09 14:39:26 -0600

    Login creado

--- app/src/main/java/com/arm/herecompanion/MainActivity.java
+++ app/src/main/java/com/arm/herecompanion/MainActivity.java
@@ -1,13 +1,27 @@
 package com.arm.herecompanion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btnEmpezar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEmpezar = (Button)findViewById(R.id.btnEmpezar);
+        btnEmpezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
