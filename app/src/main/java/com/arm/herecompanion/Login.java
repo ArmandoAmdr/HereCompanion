ad3727b79d2773e519b3b8a8c99b119a12e5607b
Armando <luis.armando.amador.duran@gmail.com>
2018-11-09 15:43:46 -0600

    Login NUEVO
    Maquetado del login

--- app/src/main/java/com/arm/herecompanion/Login.java
+++ app/src/main/java/com/arm/herecompanion/Login.java
@@ -2,8 +2,12 @@ package com.arm.herecompanion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    public Button btnIngresar, btnRegresar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
